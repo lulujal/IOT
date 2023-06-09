a701a72cb3176876cf95222050a0d5a55a94b3c5
lulujal <92596705+lulujal@users.noreply.github.com>
2023-06-09 08:22:55 +0700

    Add files via upload

---
+++ esp32_lorawan_Pompa.cpp
@@ -0,0 +1,150 @@
+
+
+#include <lorawan.h>
+
+#define SensorPin  32
+#define PUMP 25
+#define LED 12
+
+const char *devAddr = "7df2f366";
+const char *nwkSKey = "c283a2088a55ab680000000000000000";
+const char *appSKey = "0000000000000000cad1c116517735e3";
+
+
+const unsigned long interval = 10000;    // 10 s interval to send message
+unsigned long previousMillis = 0;  // will store last time message sent
+unsigned int counter = 0;     // message counter
+String dataSend = "";
+
+char myStr[50];
+byte outStr[255];
+byte recvStatus = 0;
+int port, channel, freq;
+bool newmessage = false;
+
+const sRFM_pins RFM_pins = {
+  .CS = 5,
+  .RST = 0,
+  .DIO0 = 27,
+  .DIO1 = 2,
+};
+
+void setup() {
+  // Setup loraid access
+  Serial.begin(9600);
+//   dht.begin();
+  delay(2000);
+  if (!lora.init()) {
+    Serial.println("RFM95 not detected");
+    delay(5000);
+    return;
+  }
+
+  // Set LoRaWAN Class change CLASS_A or CLASS_C
+  lora.setDeviceClass(CLASS_A);
+
+  // Set Data Rate
+  lora.setDataRate(SF10BW125);
+
+  // Set FramePort Tx
+  lora.setFramePortTx(5);
+
+  // set channel to random
+  lora.setChannel(MULTI);
+
+  // Put ABP Key and DevAddress here
+  lora.setNwkSKey(nwkSKey);
+  lora.setAppSKey(appSKey);
+  lora.setDevAddr(devAddr);
+  pinMode(PUMP, OUTPUT);
+  digitalWrite(PUMP, LOW);
+}
+
+void loop() {
+  // Check interval overflow
+  int state;
+  if (millis() - previousMillis > interval) {
+    previousMillis = millis();
+
+    unsigned int sensorValue = analogRead(SensorPin);
+    Serial.print(sensorValue);
+    if (sensorValue < 1500) {
+      Serial.print("Pompa nyala");
+      state=1;
+      digitalWrite(PUMP, HIGH);
+    } else {
+      Serial.print("Pompa mati");
+      state=0;
+      digitalWrite(PUMP, LOW);
+    }
+    Serial.print(sensorValue);
+    dataSend = "{\"ketinggian air\": " + (String)sensorValue + ", \"Pompa\": " + (String)state +"}";
+    dataSend.toCharArray(myStr,50);
+
+
+    Serial.print("Sending: ");
+    Serial.println(dataSend);
+    lora.sendUplink(myStr, strlen(myStr), 0);
+    port = lora.getFramePortTx();
+    channel = lora.getChannel();
+    freq = lora.getChannelFreq(channel);
+    Serial.print(F("fport: "));    Serial.print(port);Serial.print(" ");
+    Serial.print(F("Ch: "));    Serial.print(channel);Serial.print(" ");
+    Serial.print(F("Freq: "));    Serial.print(freq);Serial.println(" ");
+
+  }
+
+  // Check Lora RX
+  lora.update();
+
+  recvStatus = lora.readDataByte(outStr);
+  if (recvStatus) {
+    newmessage = true;
+    int counter = 0;
+    port = lora.getFramePortRx();
+    channel = lora.getChannelRx();
+    freq = lora.getChannelRxFreq(channel);
+
+    for (int i = 0; i < recvStatus; i++)
+    {
+      if (((outStr[i] >= 32) && (outStr[i] <= 126)) || (outStr[i] == 10) || (outStr[i] == 13))
+        counter++;
+    }
+    if (port != 0)
+    {
+      if (counter == recvStatus)
+      {
+        Serial.print(F("Received String : "));
+        for (int i = 0; i < recvStatus; i++)
+        {
+          Serial.print(char(outStr[i]));
+        }
+      }
+      else
+      {
+        Serial.print(F("Received Hex: "));
+        for (int i = 0; i < recvStatus; i++)
+        {
+          Serial.print(outStr[i], HEX); Serial.print(" ");
+        }
+      }
+      Serial.println();
+      Serial.print(F("fport: "));    Serial.print(port);Serial.print(" ");
+      Serial.print(F("Ch: "));    Serial.print(channel);Serial.print(" ");
+      Serial.print(F("Freq: "));    Serial.println(freq);Serial.println(" ");
+    }
+    else
+    {
+      Serial.print(F("Received Mac Cmd : "));
+      for (int i = 0; i < recvStatus; i++)
+      {
+        Serial.print(outStr[i], HEX); Serial.print(" ");
+      }
+      Serial.println();
+      Serial.print(F("fport: "));    Serial.print(port);Serial.print(" ");
+      Serial.print(F("Ch: "));    Serial.print(channel);Serial.print(" ");
+      Serial.print(F("Freq: "));    Serial.println(freq);Serial.println(" ");
+    }
+  }
+
+}
